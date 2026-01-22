2ad6fca88b0b439940505858bca26dfeb4261367
huangshijia <1055114578@qq.com>
2026-01-22 19:32:13 +0800

    support for yamux

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -116,13 +116,10 @@ impl ServerManager {
                 format!("ServerManager not running"),
             ));
         }
-        if let None = self.listener {
-            return Err(Error::other(format!("Listener not initialized")));
-        }
 
-        let transport = match self.listener.unwrap() {
+        let transport = match &self.listener {
             #[cfg(feature = "use-xtransport")]
-            Listener::XTransport(xtransport_listener) => {
+            Some(Listener::XTransport(xtransport_listener)) => {
                 let (stream, addr) = xtransport_listener.accept()?;
                 info!("Accepted xtransport connection from {:?}", addr);
 
@@ -133,16 +130,20 @@ impl ServerManager {
                 transport
             },
             #[cfg(feature = "use-yamux")]
-            Listener::Yamux(yamux_listener) => {
+            Some( Listener::Yamux(yamux_listener)) => {
                 let (stream, addr) = block_on(async{
-                    yamux_listener.accept().await?
-                });
+                    let (ret1, ret2) = yamux_listener.accept().await?;
+                    Ok::<_, std::io::Error>((ret1, ret2))
+                })?;
                 info!("Accepted yamux connection from {:?}", addr);
                 // 创建 YamuxTransport 实例并从流初始化
                 let mut transport = YamuxTransportHandler::new(yamux::Mode::Server);
                 transport.from_tokio_stream(stream)?;
                 transport
             },
+            None => {
+                return Err(Error::other(format!("Listener not initialized")));
+            }
         };
 
         Ok(VirgeServer::new(transport, true))
@@ -161,45 +162,3 @@ impl ServerManager {
         self.running
     }
 }
-
-// /// Virga 服务器连接：与VirgeClient类似，负责单个连接的数据传输。
-// pub struct VirgeServer {
-//     transport: Box<dyn Transport>,
-//     connected: bool,
-// }
-
-// impl VirgeServer {
-//     /// 发送数据
-//     pub async fn send(&mut self, data: Vec<u8>) -> Result<()> {
-//         if !self.connected {
-//             return Err(VirgeError::TransportError(
-//                 "Server not connected".to_string(),
-//             ));
-//         }
-//         self.transport.send(data).await
-//     }
-
-//     /// 接收数据
-//     pub async fn recv(&mut self) -> Result<Vec<u8>> {
-//         if !self.connected {
-//             return Err(VirgeError::TransportError(
-//                 "Server not connected".to_string(),
-//             ));
-//         }
-//         self.transport.recv().await
-//     }
-
-//     /// 断开连接
-//     pub async fn disconnect(&mut self) -> Result<()> {
-//         if self.connected {
-//             self.transport.disconnect().await?;
-//             self.connected = false;
-//         }
-//         Ok(())
-//     }
-
-//     /// 检查连接状态
-//     pub fn is_connected(&self) -> bool {
-//         self.connected && self.transport.is_connected()
-//     }
-// }
