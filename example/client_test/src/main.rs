4dcad1b2024ba03064162f335fe0cd7208c3d815
huangshijia <1055114578@qq.com>
2026-01-22 21:06:11 +0800

    add runtime

--- example/client_test/src/main.rs
+++ example/client_test/src/main.rs
@@ -10,21 +10,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = VirgeClient::new(config);
     client.connect()?;
 
-    // 处理发送数据, 先发送数据长度，然后发送数据
-    let data = vec![1; 512];
-    client.write(&data.len().to_be_bytes())?;
-    client.write(&data)?;
+    // // 处理发送数据, 先发送数据长度，然后发送数据
+    // let data = vec![1; 512];
+    // client.write(&data.len().to_be_bytes())?;
+    // client.write(&data)?;
 
 
     // 处理接收数据, 先接收数据长度，然后创建一个足够长的databuf，最后接收数据
     let mut buf = [0u8; 8];
-    client.read(&mut buf)?;
+    client.read_exact(&mut buf)?;
     let data_len = usize::from_be_bytes(buf);
     
     let mut data = vec![0; data_len];
-    let actual_data_len_ = client.read(&mut data)?;
-    assert_eq!(data_len, actual_data_len_);
-    println!("len date = {}", actual_data_len_);
+    client.read_exact(&mut data)?;
+    
+    println!("len date = {}", data_len);
     
     // 断开连接
     //client.disconnect()?;
--- example/server_test/src/main.rs
+++ example/server_test/src/main.rs
@@ -12,19 +12,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Ok(mut server) = manager.accept() {
         println!("there is a new virgeserver");
         
-        // 处理接收数据, 先接收数据长度，然后创建一个足够长的databuf，最后接收数据
-        let mut buf: [u8; 8] = [0u8; 8];
-        server.read(&mut buf)?;
-        let data_len = usize::from_be_bytes(buf);
-        println!("data_len: {data_len}");
+        // // 处理接收数据, 先接收数据长度，然后创建一个足够长的databuf，最后接收数据
+        // let mut buf: [u8; 8] = [0u8; 8];
+        // println!("init 4");
+        // server.read(&mut buf)?;
+        // println!("init 5");
+        // let data_len = usize::from_be_bytes(buf);
+        // println!("data_len: {data_len}");
         
-        let mut data = vec![0; data_len];
-        let actual_data_len_ = server.read(&mut data)?;
-        assert_eq!(data_len, actual_data_len_);
+        let mut data = vec![0; 512];
+        // server.read(&mut data)?;
+        
         
         // 处理发送数据, 先发送数据长度，然后发送数据
-        server.write_all(&data.len().to_be_bytes())?;
-        server.write_all(&data)?;
+        server.write(&data.len().to_be_bytes())?;
+        server.write(&data)?;
         
         // 断开连接
         // server.disconnect()?;
